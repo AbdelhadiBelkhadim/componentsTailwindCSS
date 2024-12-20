6d7da1e7f77c8c8687036fd0228921a6d51640de
AbdelhadiBelkhadim <belkhdim246@gmail.com>
2024-12-20 20:23:12 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    extend: {
+      colors: {
+        Primary70: '#3DCBB1',
+        Secondary: '#FFD130',
+        Primary40: '#EC9792',
+        Primary60:'#E56D67',
+        Primary80 : '#C42A22',
+        Primary100 : '#98211A',
+        Black90 : '#1B1B1BE5',
+        Black60 : '#1B1B1B99', 
+        Black30 : '#1B1B1B1A', 
+        White90 : '#F9F9F9E5', 
+        Error : '#FE416D',
+        Warning : '#A04AE3',
+        Success : '#00DDC0',
+
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
